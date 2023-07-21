b5bae733d019492e303777c491eded12fa935214
Sivabharathi-Virudhumani <sivabharathi@shivasoftwares.com>
2023-07-21 19:12:03 +0530

    Damage Entry

---
+++ ROMS/INV_DamageEntry.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ROMS
+{
+    public partial class INV_DamageEntry : Form
+    {
+        DataValidation objValidation = new DataValidation();
+        DataError objError;
+
+        public INV_DamageEntry()
+        {
+            InitializeComponent();
+        }
+
+        
+        public void udfnclose()
+        {
+            try
+            {
+                DialogResult dialogResult = MessageBox.Show("Do you want to Exit ?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dialogResult == DialogResult.Yes)
+                {
+                    this.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                objError = new DataError();
+                objError.WriteFile(ex);
+            }
+        }
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                udfnclose();  
+            }
+            catch (Exception ex)
+            {
+                objError = new DataError();
+                objError.WriteFile(ex);
+            }
+        }
+
+        private void INV_Inward_Load(object sender, EventArgs e)
+        {
+            if (btnSave.Text == "Save")
+            {
+                grpproductname.Visible = true;
+                txtsuppliername.Enabled = true;
+                
+            }
+            else {
+                grpproductname.Visible = false;
+                txtsuppliername.Enabled = false;
+                //cmbvoucherno.Enabled = false;
+                //cmbPoNo.Enabled = false;
+                //cmbinwardtype.Enabled = false;
+            }
+        }
+
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Txtsuppliername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btnsaveasdraft_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
