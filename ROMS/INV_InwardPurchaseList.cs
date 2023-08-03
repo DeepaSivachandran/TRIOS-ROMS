abd89c47f4f0602339109f49221e9754af1801b5
deepa <deepa@shivasoftwares.com>
2023-08-03 10:19:28 +0530

    puchase list changes

--- ROMS/INV_InwardPurchaseList.cs
+++ ROMS/INV_InwardPurchaseList.cs
@@ -350,7 +350,7 @@ namespace ROMS
             {
                 if (((Control.ModifierKeys & Keys.Control) == Keys.Control) && (e.KeyCode == Keys.N))
                 {
-                    tsbNew_Click(sender, e);
+                    //tsbNew_Click(sender, e);
                 }
                 if (((Control.ModifierKeys & Keys.Control) == Keys.Control) && (e.KeyCode == Keys.E))
                 {
