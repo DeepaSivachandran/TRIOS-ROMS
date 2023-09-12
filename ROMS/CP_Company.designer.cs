f3eaf2a3c0d03dfcbecebfd3aa3f98382f7c6267
deepa <deepa@shivasoftwares.com>
2023-09-12 09:23:54 +0530

    company changes

--- ROMS/CP_Company.designer.cs
+++ ROMS/CP_Company.designer.cs
@@ -1348,7 +1348,7 @@
             // 
             this.txtName.Font = new System.Drawing.Font("Oswald Regular", 10.75F);
             this.txtName.Location = new System.Drawing.Point(13, 39);
-            this.txtName.MaxLength = 500;
+            this.txtName.MaxLength = 50;
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(189, 27);
             this.txtName.TabIndex = 0;
