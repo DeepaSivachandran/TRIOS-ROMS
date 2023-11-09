d6bd72eadec105240086881bdefbfde76c875fa5
deepa <deepa@shivasoftwares.com>
2023-11-09 18:18:11 +0530

    PO CHANGES

--- ROMS/PUR_PurchaseOrder.cs
+++ ROMS/PUR_PurchaseOrder.cs
@@ -1967,8 +1967,7 @@ namespace ROMS
                 DataSet objDs = new DataSet();
                 if (txtProductName.Text.Length > 0)
                 {
-                    objDs = objspdservice.udfnproductmasterlist(29, 0, 0, 0, 0, txtProductName.Text, "", "", 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, txtProductName.Text, Convert.ToInt32(lblSupplierCode.Text), varProductsCodes,null);
-                    objspdservice.CloseConnection();
+                    objDs = objspdservice.udfnproductmasterlist(29, 0, 0, 0, 0, txtProductName.Text, "", "", Convert.ToInt32(cmbConcern.SelectedValue), 0, 0, Convert.ToInt32(lblschedule.Text), 0, 0, 0, 0, 0, 0, 0, 0, 0, txtProductName.Text, Convert.ToInt32(lblSupplierCode.Text), varProductsCodes,null);
                     if (objDs != null)
                     {
                         if (objDs.Tables.Count != 0)
