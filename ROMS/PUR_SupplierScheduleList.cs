ac60b79edaf213f11d11d7908a11ee323cbdff17
MSVenkatakumar <109940926+MSVenkatakumar@users.noreply.github.com>
2023-12-08 19:22:34 +0530

    supplier

--- ROMS/PUR_SupplierScheduleList.cs
+++ ROMS/PUR_SupplierScheduleList.cs
@@ -1971,7 +1971,7 @@ namespace ROMS
                         ep_Supplierlist.Clear();
                         lblSupplierCode.Text = values[0];
                         lblschedule.Text = values[1];
-                        txtSupplier.BackColor = Color.White;
+                        txtSupplier.BackColor = Color.White; 
 
                     }
                     //VarPrevSupplierid = Convert.ToInt32(lblSupplierCode.Text);
@@ -2021,7 +2021,6 @@ namespace ROMS
                     lblNoRecordsFound.Visible = true;
                     lblNoRecordsFound.BringToFront();
                     dgvSupplierScheduleList.DataSource = null;
-
                 }
             }
             catch (Exception ex)
