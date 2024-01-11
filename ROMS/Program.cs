cf6b085a48542d5c2a213a7731e01b1ace272572
deepa <deepa@shivasoftwares.com>
2024-01-11 13:53:02 +0530

    version changed

--- ROMS/Program.cs
+++ ROMS/Program.cs
@@ -22,7 +22,7 @@ namespace ROMS
             {
                 SecurityController _security = new SecurityController();
                 //  DataService objDser = new DataService();
-                string version = "v1.4.0";
+                string version = "v1.5.0";
               //  string path = Application.StartupPath + "\\Server Settings\\serversettings.txt";
                 //if (File.Exists(pa th))
                 //{
