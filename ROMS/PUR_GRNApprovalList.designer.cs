4b1220fe4d78c7ca115f1a1943c631e8b6dd0ccf
deepa <deepa@shivasoftwares.com>
2024-02-02 10:44:01 +0530

    grn columnwidth change event

--- ROMS/PUR_GRNApprovalList.designer.cs
+++ ROMS/PUR_GRNApprovalList.designer.cs
@@ -480,7 +480,7 @@
             this.DGV_SearchGrid.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.DGV_SearchGrid_CellEndEdit);
             this.DGV_SearchGrid.CellPainting += new System.Windows.Forms.DataGridViewCellPaintingEventHandler(this.DGV_SearchGrid_CellPainting);
             this.DGV_SearchGrid.ColumnHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.DGV_SearchGrid_ColumnHeaderMouseClick);
-            this.DGV_SearchGrid.ColumnWidthChanged += new System.Windows.Forms.DataGridViewColumnEventHandler(this.DGV_SearchGrid_ColumnWidthChanged);
+           // this.DGV_SearchGrid.ColumnWidthChanged += new System.Windows.Forms.DataGridViewColumnEventHandler(this.DGV_SearchGrid_ColumnWidthChanged);
             this.DGV_SearchGrid.CurrentCellDirtyStateChanged += new System.EventHandler(this.DGV_SearchGrid_CurrentCellDirtyStateChanged);
             this.DGV_SearchGrid.EditingControlShowing += new System.Windows.Forms.DataGridViewEditingControlShowingEventHandler(this.DGV_SearchGrid_EditingControlShowing);
             this.DGV_SearchGrid.Scroll += new System.Windows.Forms.ScrollEventHandler(this.DGV_SearchGrid_Scroll);
@@ -538,7 +538,7 @@
             this.grdGrnApprovalList.Size = new System.Drawing.Size(1348, 502);
             this.grdGrnApprovalList.TabIndex = 958797;
             this.grdGrnApprovalList.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.GrdGrnApprovalList_CellDoubleClick);
-            this.grdGrnApprovalList.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.GrdGrnApprovalList_DataBindingComplete);
+      //      this.grdGrnApprovalList.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.GrdGrnApprovalList_DataBindingComplete);
             this.grdGrnApprovalList.Scroll += new System.Windows.Forms.ScrollEventHandler(this.GrdGrnApprovalList_Scroll);
             this.grdGrnApprovalList.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GrdGrnApprovalList_KeyDown);
             // 
