877e3b5eeaaae8ed35f74ddb25a431a0072065bf
KavithaParasuramanR <154586359+KavithaParasuramanR@users.noreply.github.com>
2024-02-02 16:26:19 +0530

    changes

--- ROMS/CP_UserCategory.cs
+++ ROMS/CP_UserCategory.cs
@@ -331,7 +331,7 @@ namespace ROMS
             {
                 if (varUpdate == 0)
                 {
-                    DialogResult dialogResult = MessageBox.Show("Do you want to Exit ?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    DialogResult dialogResult = MessageBox.Show("Do you want to exit ?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
                     if (dialogResult == DialogResult.Yes)
                     {
                         e.Cancel = false;
