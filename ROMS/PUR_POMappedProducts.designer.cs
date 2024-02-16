4a4c380c6ffb961499e074365e34c896a96a0f46
sathishkumar <sathishkumar@shivasoftwares.com>
2024-02-16 16:27:07 +0530

    Product Show in PO Screen

--- ROMS/PUR_POMappedProducts.designer.cs
+++ ROMS/PUR_POMappedProducts.designer.cs
@@ -257,7 +257,7 @@
             dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black;
             this.DGV_SearchGrid.RowsDefaultCellStyle = dataGridViewCellStyle3;
             this.DGV_SearchGrid.RowTemplate.Height = 25;
-            this.DGV_SearchGrid.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.DGV_SearchGrid.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
             this.DGV_SearchGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
             this.DGV_SearchGrid.ShowRowErrors = false;
             this.DGV_SearchGrid.Size = new System.Drawing.Size(965, 56);
