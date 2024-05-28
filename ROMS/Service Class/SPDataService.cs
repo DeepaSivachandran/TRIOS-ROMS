652d4e1779d138273d4fa8a17b38c5f533bf7605
kavithapr <kavithapr@DESKTOP-92UMSG1>
2024-05-28 18:10:35 +0530

    Big fixing points

--- ROMS/Service Class/SPDataService.cs
+++ ROMS/Service Class/SPDataService.cs
@@ -3228,6 +3228,7 @@ namespace ROMS
                 varSqlCommand.Parameters.AddWithValue("@paraRefreshFlag", objTRN_PurchaseEntry.paraRefreshFlag);
                 varSqlCommand.Parameters.AddWithValue("@paraTinFlag", objTRN_PurchaseEntry.paraTinFlag);
                 varSqlCommand.Parameters.AddWithValue("@paraPOID", objTRN_PurchaseEntry.paraPOID);
+                varSqlCommand.Parameters.AddWithValue("@paraUnapprovedby", objTRN_PurchaseEntry.paraUnapprovedby);
                 varSqlCommand.Parameters.AddWithValue("@ParaTRN_Purchase_Products_Details", objTRN_PurchaseEntry.Purchase_Products_Details);
                 varSqlCommand.CommandTimeout = 0;
                 result = varSqlCommand.ExecuteScalar().ToString();
