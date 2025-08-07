e145398cf1ea2c30a5d7be5d40939ee5b883afdb
sivabharathi <sivabharathi@DESKTOP-T85QVCI>
2025-08-07 12:49:15 +0530

    HSN Save

--- ROMS/CP_Product.cs
+++ ROMS/CP_Product.cs
@@ -62,6 +62,7 @@ namespace ROMS
 
         public int varGroupCode = 0, varSubgroupCode = 0, varUnitCode = 0, varbrandcode = 0, varGroupId = 0, varSubGroupId = 0, varHsnId = 0, varUnitid = 0, varcompanyid = 0, varBrandId = 0, varBatchCode = 0, varPURSLID = 0, varPURRKID = 0, varSALESLID = 0, varSALERKID = 0, pbCloneFlag = 0, varPurHSNID = 0, varSalesHSNID = 0, varPurEffectiveFromErr = 0, varSalesEffectiveFromErr = 0;
         public string varSubGroupName = "", varGroupName = "", varPurchaseLocation = "", varSalesLocation = "", varPurchaseRack = "", varMasterType = "0", varSalesRack = "", varBrandName = "", varRackDescription = "", varEname = "", varGRNid = "0", varNewproid = "0", varPurHSNCode = "", varPurGST = "", varSalesHSNCode = "", varSalesGST = "";
+        int varF5Flag = 0;
         
         public CP_Product()
         {
@@ -133,14 +134,22 @@ namespace ROMS
                     errItems.SetError(txtBarcode, "Please enter valid barcode");
                     return;
                 }
-                udfnSave(0);
+                if (varF5Flag == 0)
+                { udfnSave(0); }
+                else
+                {
+                    if(varproductcode==0)
+                    { udfnSave(0); }
+                    else
+                    { udfnSave(1); }
+                }
             }
             catch (Exception ex)
             {
                 objError = new DataError();
                 objError.WriteFile(ex);
             }
-
+            finally { varF5Flag = 0; }
         }
         public void udfnSave(int varFlag)
         {
@@ -7630,6 +7639,7 @@ namespace ROMS
                 if (e.KeyCode == Keys.F5)
                 {
                     btnSave_Click(sender, e);
+                    varF5Flag = 1;
                 }
 
             }
