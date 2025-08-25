03a70d2d8338cda10a7188b6cfbdc33f47010674
sivabharathi <sivabharathi@DESKTOP-T85QVCI>
2025-08-25 17:51:51 +0530

    commit

--- ROMS/PAY_SupplierPaymentList.cs
+++ ROMS/PAY_SupplierPaymentList.cs
@@ -732,6 +732,7 @@ namespace ROMS
                 objError.WriteFile(ex);
             }
         }
+
         public void udfnList()
         {
             try
@@ -855,9 +856,16 @@ namespace ROMS
                 DGV_SearchGrid.Columns["PAY_PaymentMode"].Visible = false;
                 DGV_SearchGrid.Columns["PAYID"].Visible = false;
                 DGV_SearchGrid.Columns["Status"].Width = 150;
+                DGV_SearchGrid.Columns["PAY_PaymentMode"].Visible = false;
+                DGV_SearchGrid.Columns["PAYID"].Visible = false;
                 DGV_SearchGrid.Columns["PAY_STSID"].Visible = false;
                 DGV_SearchGrid.Columns["PAY_BankID"].Visible = false;
                 DGV_SearchGrid.Columns["ChequeDate"].Visible = false;
+                DGV_SearchGrid.Columns["PAY_SPID"].Visible = false;
+                DGV_SearchGrid.Columns["PAY_SPSCID"].Visible = false;
+                DGV_SearchGrid.Columns["PAY_Bank_Tx_Date"].Visible = false;
+                DGV_SearchGrid.Columns["RPTName"].Visible = false;
+                DGV_SearchGrid.Columns["PrintFlag"].Visible = false;
                 DGV_SearchGrid.ScrollBars = ScrollBars.Both;
             }
             catch (Exception ex)
