2cf0a22b745895be00366538693322668b5cbc2b
sivabharathi <sivabharathi@DESKTOP-T85QVCI>
2025-09-16 10:54:21 +0530

    Product - Clone teller issue

--- ROMS/CP_Product.cs
+++ ROMS/CP_Product.cs
@@ -3775,6 +3775,10 @@ namespace ROMS
                     cbCompleted.Checked = false;
                     cbCompleted.Enabled = true;
                     pbFormStatus = 71;
+                    varStatusFlag = 0;
+                    txtTeller.Text = "";
+                    txtTeller.Enabled = false;
+                    txtTeller.ReadOnly = true;
                     this.ActiveControl = txtPICode;
                 }
                 //if (btnSave.Text != "Update")
