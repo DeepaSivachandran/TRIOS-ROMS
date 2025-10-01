1fb0ad380925c030b1013778bfb6490e780728e8
Sathish-PremKumar <139870559+Sathis-PremKumar@users.noreply.github.com>
2025-10-01 12:42:52 +0530

    City autocomplete Changes

--- ROMS/REPORT_Supplier_Payment.cs
+++ ROMS/REPORT_Supplier_Payment.cs
@@ -1103,7 +1103,7 @@ namespace ROMS
                     DataSet objDs = new DataSet();
                     if (txtCity.Text.Length > 0)
                     {
-                        objDs = objspdservice.udfnCitylist(1, txtCity.Text, 0, 0);
+                        objDs = objspdservice.udfnCitylist(4, txtCity.Text, 0, 0);
                         objspdservice.CloseConnection();
                         if (objDs != null)
                         {
