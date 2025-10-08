652550c1e06c749f029490646b416b98bd0a28d5
Deepa Sivachandran <Deepa Sivachandran@DEEPA>
2025-10-08 16:09:11 +0530

    toolstrip item removed

--- ROMS/MainForm.designer.cs
+++ ROMS/MainForm.designer.cs
@@ -193,7 +193,6 @@ namespace ROMS
             this.tsmClearMasters = new System.Windows.Forms.ToolStripMenuItem();
             this.tsmFinancialYearProcess = new System.Windows.Forms.ToolStripMenuItem();
             this.ms = new System.Windows.Forms.MenuStrip();
-            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.ms.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -1680,8 +1679,7 @@ namespace ROMS
             this.tsmTally,
             this.tsmReports,
             this.tsmMyProfile,
-            this.tsmFYSettings,
-            this.toolStripMenuItem1});
+            this.tsmFYSettings});
             this.ms.Location = new System.Drawing.Point(0, 0);
             this.ms.Name = "ms";
             this.ms.Padding = new System.Windows.Forms.Padding(5, 2, 0, 2);
@@ -1689,11 +1687,6 @@ namespace ROMS
             this.ms.TabIndex = 112;
             this.ms.Text = "ms";
             this.ms.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.Ms_ItemClicked);
-            // 
-            // toolStripMenuItem1
-            // 
-            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
-            this.toolStripMenuItem1.Size = new System.Drawing.Size(12, 21);
             // 
             // MainForm
             // 
