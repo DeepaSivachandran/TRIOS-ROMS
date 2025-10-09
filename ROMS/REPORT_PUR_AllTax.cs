5e5ba28560bbd9a7be3313e9cf04fd718115c137
Sathish-PremKumar <139870559+Sathis-PremKumar@users.noreply.github.com>
2025-10-09 21:51:21 +0530

    All Purchase Daywise

--- ROMS/REPORT_PUR_AllTax.cs
+++ ROMS/REPORT_PUR_AllTax.cs
@@ -234,6 +234,27 @@ namespace ROMS
                     objBillreport.SetParameterValue("paraSupplierID", 0);
                     objBillreport.SetParameterValue("paraScheduleID", 0);
 
+
+                    objBillreport.SetParameterValue("paraSupplierType", 0, objBillreport.Subreports[0].Name.ToString());
+                    objBillreport.SetParameterValue("paraHSNCode", 0, objBillreport.Subreports[0].Name.ToString());
+                    objBillreport.SetParameterValue("paraGST", Convert.ToInt32(cmbGST.SelectedValue), objBillreport.Subreports[0].Name.ToString());
+                    objBillreport.SetParameterValue("paraCompanyId", 0, objBillreport.Subreports[0].Name.ToString());
+                    objBillreport.SetParameterValue("paraInvioceType", 0, objBillreport.Subreports[0].Name.ToString());
+                    objBillreport.SetParameterValue("paraPaymentType", 0, objBillreport.Subreports[0].Name.ToString());
+                    objBillreport.SetParameterValue("paraPurchaseType", 0, objBillreport.Subreports[0].Name.ToString());
+                    objBillreport.SetParameterValue("paraConditionType", 0, objBillreport.Subreports[0].Name.ToString());
+                    objBillreport.SetParameterValue("paraBrandID", 0, objBillreport.Subreports[0].Name.ToString());
+                    objBillreport.SetParameterValue("paraAlpha", "", objBillreport.Subreports[0].Name.ToString());
+                    objBillreport.SetParameterValue("paraFromDate", dpFromDate.Text, objBillreport.Subreports[0].Name.ToString());
+                    objBillreport.SetParameterValue("paraToDate", dpToDate.Text, objBillreport.Subreports[0].Name.ToString());
+                    objBillreport.SetParameterValue("paraProductNameType", 0, objBillreport.Subreports[0].Name.ToString());
+                    objBillreport.SetParameterValue("paraGroupId", 0, objBillreport.Subreports[0].Name.ToString());
+                    objBillreport.SetParameterValue("paraSubgroupId", 0, objBillreport.Subreports[0].Name.ToString());
+                    objBillreport.SetParameterValue("paraProductId", 0, objBillreport.Subreports[0].Name.ToString());
+                    objBillreport.SetParameterValue("paraSupplierID", 0, objBillreport.Subreports[0].Name.ToString());
+                    objBillreport.SetParameterValue("paraScheduleID", 0, objBillreport.Subreports[0].Name.ToString());
+
+
                     objBillreport.SetParameterValue("paraHostName", MainForm.pbHostName);
                     objBillreport.SetParameterValue("paraUserName", MainForm.pbUserName);
                     objValidation.CrySqlConnection(objBillreport);
