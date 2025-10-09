abccea91005ffd6b16e26423461ffffb79d570f9
sivabharathi <sivabharathi@DESKTOP-T85QVCI>
2025-10-09 17:42:37 +0530

    Po schedule access

--- ROMS/REPORT_PriceList.cs
+++ ROMS/REPORT_PriceList.cs
@@ -285,10 +285,18 @@ namespace ROMS
             {
                 dynamicLabelControl.PlaceholderLabel = tsLabelPlaceholder;
                 int currentMUCode = 80315;
+                string ReportTypeIDs = string.Join(",",
+                 MainForm.objDtMenuDetailsUser?.AsEnumerable()
+                  .Where(r => r.Field<int?>("MU_ParentMenuCode") == currentMUCode)
+                  .Select(r => r.Field<int?>("MU_EQID"))
+                  .Where(q => q.HasValue)
+                  .Select(q => q.Value.ToString())
+                  ?? Enumerable.Empty<string>());
                 dynamicLabelControl.BindMenuHierarchy(currentMUCode);
                 DataBind objDataBind = new DataBind();
                 objDataBind.BindComboBoxListSelected("MR_Company", "COM_STSID in(1,2) and COMID !=-1 Order by COMID", "COM_ShortName,COMID", cmbConcern, "", "COM_ShortName", "COMID");
-                objDataBind.BindComboBoxListSelected("DEF_MASTER", "MST_TransactionID IN (124) AND MSTID<>0 ORDER BY MST_OrderID ASC ", "MST_DisplayText,MSTID,MST_ShortName", cmbReportType, "", "MST_DisplayText", "MSTID");
+                objDataBind.BindComboBoxListSelected("DEF_MASTER", "MST_TransactionID IN (0) AND MSTID<>0 OR MSTID IN (" + ReportTypeIDs + ")  ORDER BY MST_OrderID ASC ",
+                      "MST_DisplayText,MSTID,MST_ShortName", cmbReportType, "", "MST_DisplayText", "MSTID");
                 objDataBind.BindComboBoxListSelected("DEF_MASTER", "MST_TransactionID IN (125,0) AND MSTID<>-1", "MST_DisplayText,MSTID", cmbRateType, "", "MST_DisplayText", "MSTID");
                 objDataBind = null;
 
---
+++ ROMS/Service Class/UserAccessHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+namespace ROMS
+{
+    class UserAccessHelper
+    {
+        public static HashSet<string> _accessLevels;
+       // public static string PrivilegeCode, SpecialPermissionCode;
+
+        public static (string PrivilegeCode, List<(int MUP_Code, string EditAccess)> SpecialPermissions)  LoadUserAccess(int menuCode)
+        {
+            //if (MainForm.objDtMenuDetailsUser == null)
+            //{
+            //    _accessLevels = new HashSet<string>();
+            //    return;
+            //}
+            string PrivilegeCode, SpecialPermissionCode;
+            
+                 PrivilegeCode = string.Join(",",
+                 MainForm.objDtMenuDetailsUser?.AsEnumerable()
+                 .Where(r => r != null
+                      && r.Table.Columns.Contains("MU_Code")
+                      && r.Table.Columns.Contains("URM_Access_Level")
+                      && !r.IsNull("MU_Code")
+                      && r.Field<int?>("MU_Code") == menuCode
+                      && !r.IsNull("URM_Access_Level"))
+                 .Select(r => r.Field<string>("URM_Access_Level")) 
+                  ?? Enumerable.Empty<string>()
+                  );
+ 
+               var SpecialPermissions = MainForm.objDtMenuSplPermissionUser?
+              .AsEnumerable()
+              .Where(r => r.Field<int?>("MUP_MU_Code") == menuCode
+                          && !r.IsNull("EditAccess")
+                          && !r.IsNull("MUP_Code"))
+              .Select(r => (
+                  MUP_Code: r.Field<int>("MUP_Code"),
+                  EditAccess: r.Field<string>("EditAccess")
+              ))
+              .ToList() ?? new List<(int, string)>();
+             
+            return (PrivilegeCode, SpecialPermissions);
+             
+        }
+
+    }
+}
