70176d6b3010c82d9e1b8425603fb186632207f2
Sathish-PremKumar <139870559+Sathis-PremKumar@users.noreply.github.com>
2025-10-15 13:08:45 +0530

    Changes

--- ROMS/User Controls/DynamicWindowControl.cs
+++ ROMS/User Controls/DynamicWindowControl.cs
@@ -33,7 +33,7 @@ namespace ROMS
             // --- Minimize Label ---
             MinimizeLabel = new ToolStripLabel()
             {
-                Image = Properties.Resources.mismatch,
+                Image = Properties.Resources.minimize_Form,
                 DisplayStyle = ToolStripItemDisplayStyle.Image,
                 Margin = new Padding(4, 0, 4, 0),
                 Alignment = ToolStripItemAlignment.Right
