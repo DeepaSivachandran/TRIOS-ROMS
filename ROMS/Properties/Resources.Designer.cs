dda7e36eff5ec43a99c0b704d82927c5e9950736
Sathish-PremKumar <139870559+Sathis-PremKumar@users.noreply.github.com>
2025-10-15 13:03:31 +0530

    Changes

--- ROMS/Properties/Resources.Designer.cs
+++ ROMS/Properties/Resources.Designer.cs
@@ -273,26 +273,6 @@ namespace ROMS.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap close_window {
-            get {
-                object obj = ResourceManager.GetObject("close-window", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap close_window1 {
-            get {
-                object obj = ResourceManager.GetObject("close-window1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap comment {
             get {
                 object obj = ResourceManager.GetObject("comment", resourceCulture);
