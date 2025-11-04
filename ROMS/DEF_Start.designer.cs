c6315bb36bb3ac88c5a4c26a4de54b7b5d699088
Sathish-PremKumar <139870559+Sathis-PremKumar@users.noreply.github.com>
2025-11-04 09:57:46 +0530

    Def_Start Size Change

--- ROMS/DEF_Start.designer.cs
+++ ROMS/DEF_Start.designer.cs
@@ -32,11 +32,11 @@
             // 
             // DEF_Start
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 19F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImage = global::ROMS.Properties.Resources.Launcher;
-            this.ClientSize = new System.Drawing.Size(1354, 675);
+            this.ClientSize = new System.Drawing.Size(1350, 670);
             this.Font = new System.Drawing.Font("Oswald Regular", 10.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.KeyPreview = true;
