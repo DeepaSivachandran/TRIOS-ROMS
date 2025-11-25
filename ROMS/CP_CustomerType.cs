b5ad1364bf0894f984413953db35a8113a930db1
Sathish-PremKumar <139870559+Sathis-PremKumar@users.noreply.github.com>
2025-11-25 18:59:57 +0530

    Changes

---
+++ ROMS/CP_CustomerType.cs
@@ -0,0 +1,323 @@
+﻿using ROMS.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ROMS
+{
+    public partial class CP_CustomerType : Form
+    {
+        DataValidation objValidation = new DataValidation();
+        DataError objError;
+        private ToolTip tpCancel = new ToolTip();
+        public string varSupplierIds, varGIId="0";
+        public int varPrid=0,varFlag=0;
+        public CP_CustomerType()
+        {
+            InitializeComponent();
+            
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtCount.Text.Trim() == "")
+                {
+                    errBrand.SetError(txtCount, "Please enter no. of copies.");
+                    txtCount.BackColor = System.Drawing.ColorTranslator.FromHtml("#fabdbd");
+                    tpCancel.ShowAlways = true;
+                    tpCancel.Show("Please enter no. of copies.", txtCount, 5000);
+                    return;
+                }
+                else
+                {
+                    if (Convert.ToInt32(txtCount.Text.Trim()) < 1)
+                    {
+                        errBrand.SetError(txtCount, "Please enter valid no. of copies.");
+                        txtCount.BackColor = System.Drawing.ColorTranslator.FromHtml("#fabdbd");
+                        tpCancel.ShowAlways = true;
+                        tpCancel.Show("Please enter valid no. of copies.", txtCount, 5000);
+                        return;
+                    }
+                }
+                if (varFlag == 1) ///inward sticker print count
+                {
+                    udfnStickerPrint();
+                }
+                if (varFlag == 2) ///inward sticker print count
+                {
+                    udfnStickerFormPurchasePrint();
+                }
+                else { 
+                    udfnPrint();
+                }
+            }
+            catch (Exception ex)
+            {
+                objError = new DataError();
+                objError.WriteFile(ex);
+            }
+        }
+        public void udfnPrint()
+        {
+            try
+            {
+                errBrand.Clear();
+                SPDataService objSPdataservice = new SPDataService();
+                DataSet objDs = new DataSet();
+                CrystalDecisions.CrystalReports.Engine.ReportDocument objBillreport = new CrystalDecisions.CrystalReports.Engine.ReportDocument();
+                objBillreport = new CrystalDecisions.CrystalReports.Engine.ReportDocument();
+                MR_Supplier objMR_Supplier = new MR_Supplier();
+                objMR_Supplier.ViewType = 42;
+                objMR_Supplier.paraSupplierIds = varSupplierIds;
+                objMR_Supplier.paraStickerCount = Convert.ToInt32(txtCount.Text.Trim());
+                objDs = objSPdataservice.udfnSupplierList(objMR_Supplier);
+                objSPdataservice.CloseConnection();
+                if (objDs != null)
+                {
+                    objBillreport.Load(Application.StartupPath + "\\Reports\\RPT_Supplier_Envelope.rpt");
+                    objBillreport.SetParameterValue("paraSupplierIds", varSupplierIds);
+                    objBillreport.SetParameterValue("paraStickerCount", Convert.ToInt32(txtCount.Text.Trim()));
+                    objValidation.CrySqlConnection(objBillreport);
+                    MainForm.objReportLoad = new ReportLoad();
+                    MainForm.objReportLoad.cryptview.ReportSource = objBillreport;
+                    MainForm.objReportLoad.ShowDialog();
+                }
+                else
+                {
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                objError = new DataError();
+                objError.WriteFile(ex);
+            }
+            finally
+            {
+                this.Close();
+            }
+        }
+
+
+        public void udfnStickerPrint()
+        {
+            try
+            {
+                if (varGIId != "0")
+                {
+                    string varHeader = "";
+                    CrystalDecisions.CrystalReports.Engine.ReportDocument objBillreport = new CrystalDecisions.CrystalReports.Engine.ReportDocument();
+                    objBillreport = new CrystalDecisions.CrystalReports.Engine.ReportDocument();
+                    objBillreport.Load(Application.StartupPath + "\\Reports\\RPT_Inward_Sticker_Print_100x70.rpt");
+
+                    objBillreport.SetParameterValue("paraGIID", Convert.ToInt32(varGIId));
+                    objBillreport.SetParameterValue("paraPRID", Convert.ToInt32(varPrid));
+                    objBillreport.SetParameterValue("paraStickerCount", Convert.ToInt32(txtCount.Text.Trim()));
+                    objBillreport.SetParameterValue("paraFlag", 2); ////item wise sticker print  for inward
+                    objBillreport.SetParameterValue("paraHostName", MainForm.pbHostName);
+                    objBillreport.SetParameterValue("paraUserName", MainForm.pbUserName);
+                    objValidation.CrySqlConnection(objBillreport);
+
+                    MainForm.objReportLoad = new ReportLoad();
+                    MainForm.objReportLoad.cryptview.ReportSource = objBillreport;
+                    MainForm.objReportLoad.Text = varHeader;
+                    MainForm.objReportLoad.ShowDialog();
+                }
+                else {
+                    return;
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                objError = new DataError();
+                objError.WriteFile(ex);
+            }
+            finally
+            {
+                this.Close();
+            }
+        }
+
+        public void udfnStickerFormPurchasePrint()
+        {
+            try
+            {
+                if (varGIId != "0")
+                {
+                    string varHeader = "";
+                    CrystalDecisions.CrystalReports.Engine.ReportDocument objBillreport = new CrystalDecisions.CrystalReports.Engine.ReportDocument();
+                    objBillreport = new CrystalDecisions.CrystalReports.Engine.ReportDocument();
+                    objBillreport.Load(Application.StartupPath + "\\Reports\\RPT_Inward_FPurchase_Sticker_Print_100x70.rpt");
+
+                    objBillreport.SetParameterValue("paraGIID", Convert.ToInt32(varGIId));
+                    objBillreport.SetParameterValue("paraPRID", Convert.ToInt32(varPrid));
+                    objBillreport.SetParameterValue("paraStickerCount", Convert.ToInt32(txtCount.Text.Trim()));
+                    objBillreport.SetParameterValue("paraFlag", 2); ////item wise sticker print  for inward
+                    objBillreport.SetParameterValue("paraHostName", MainForm.pbHostName);
+                    objBillreport.SetParameterValue("paraUserName", MainForm.pbUserName);
+                    objValidation.CrySqlConnection(objBillreport);
+
+                    MainForm.objReportLoad = new ReportLoad();
+                    MainForm.objReportLoad.cryptview.ReportSource = objBillreport;
+                    MainForm.objReportLoad.Text = varHeader;
+                    MainForm.objReportLoad.ShowDialog();
+                }
+                else
+                {
+                    return;
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                objError = new DataError();
+                objError.WriteFile(ex);
+            }
+            finally
+            {
+                this.Close();
+            }
+        }
+
+        
+        private void btnSave_Enter(object sender, EventArgs e)
+        {
+            try
+            {
+                btnSave.BackColor = Color.LemonChiffon;
+            }
+            catch (Exception ex)
+            {
+                objError = new DataError();
+                objError.WriteFile(ex);
+            }
+        }
+        private void btnSave_Leave(object sender, EventArgs e)
+        {
+            try
+            {
+                btnSave.BackColor = Color.Transparent;
+            }
+            catch (Exception ex)
+            {
+                objError = new DataError();
+                objError.WriteFile(ex);
+            }
+        }
+        private void CP_Brand_Leave(object sender, EventArgs e)
+        {
+            try
+            {
+                tpCancel.Active = false;
+            }
+            catch (Exception ex)
+            {
+                objError = new DataError();
+                objError.WriteFile(ex);
+            }
+        }
+
+        private void CP_Brand_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.KeyCode == Keys.F5)
+                {
+                    btnSave_Click(sender, e);
+                }
+            }
+            catch (Exception ex)
+            {
+                objError = new DataError();
+                objError.WriteFile(ex);
+            }
+        }
+
+        private void CancelReason_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Text = "Supplier Envelope Label";
+                if (varFlag == 1) ///inward sticker print count
+                {
+                    this.Text = "Inward Label print";
+                }
+
+                txtCount.Focus();
+            }
+            catch (Exception ex)
+            {
+                objError = new DataError();
+                objError.WriteFile(ex);
+            }
+        }
+
+        private void TxtReason_Enter(object sender, EventArgs e)
+        {
+            try
+            {
+                txtCount.BackColor = Color.LemonChiffon;
+            }
+            catch (Exception ex)
+            {
+                objError = new DataError();
+                objError.WriteFile(ex);
+            }
+        }
+
+        private void TxtReason_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    btnSave.Focus();
+                }
+            }
+            catch (Exception ex)
+            {
+                objError = new DataError();
+                objError.WriteFile(ex);
+            }
+        }
+
+        private void TxtReason_Leave(object sender, EventArgs e)
+        {
+            try
+            {
+                txtCount.BackColor = Color.White;
+            }
+            catch (Exception ex)
+            {
+                objError = new DataError();
+                objError.WriteFile(ex);
+            }
+        }
+
+        private void TxtCount_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+                {
+                    e.Handled = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                objError = new DataError();
+                objError.WriteFile(ex);
+            }
+        }
+    }
+}
---
+++ ROMS/CP_CustomerType.designer.cs
@@ -0,0 +1,218 @@
+﻿namespace ROMS
+{
+    partial class CP_CustomerType
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CP_CustomerType));
+            this.txtCount = new System.Windows.Forms.TextBox();
+            this.errBrand = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.pnlStatus = new System.Windows.Forms.Panel();
+            this.rbActive = new System.Windows.Forms.RadioButton();
+            this.rbInActive = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.grbForm = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.errBrand)).BeginInit();
+            this.pnlStatus.SuspendLayout();
+            this.grbForm.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtCount
+            // 
+            this.txtCount.Font = new System.Drawing.Font("Oswald Regular", 10.75F);
+            this.txtCount.Location = new System.Drawing.Point(113, 23);
+            this.txtCount.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtCount.MaxLength = 50;
+            this.txtCount.Name = "txtCount";
+            this.txtCount.Size = new System.Drawing.Size(210, 27);
+            this.txtCount.TabIndex = 0;
+            this.txtCount.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtCount.Enter += new System.EventHandler(this.TxtReason_Enter);
+            this.txtCount.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TxtReason_KeyDown);
+            this.txtCount.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtCount_KeyPress);
+            this.txtCount.Leave += new System.EventHandler(this.TxtReason_Leave);
+            // 
+            // errBrand
+            // 
+            this.errBrand.ContainerControl = this;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Oswald Regular", 10.75F);
+            this.btnSave.Image = global::ROMS.Properties.Resources.save;
+            this.btnSave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSave.Location = new System.Drawing.Point(169, 88);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(74, 34);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            this.btnSave.Enter += new System.EventHandler(this.btnSave_Enter);
+            this.btnSave.Leave += new System.EventHandler(this.btnSave_Leave);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.BackColor = System.Drawing.SystemColors.Control;
+            this.txtStatus.Enabled = false;
+            this.txtStatus.Font = new System.Drawing.Font("Oswald Regular", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtStatus.Location = new System.Drawing.Point(6, 50);
+            this.txtStatus.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.ReadOnly = true;
+            this.txtStatus.Size = new System.Drawing.Size(107, 28);
+            this.txtStatus.TabIndex = 11;
+            this.txtStatus.Text = "Status";
+            // 
+            // pnlStatus
+            // 
+            this.pnlStatus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlStatus.Controls.Add(this.rbActive);
+            this.pnlStatus.Controls.Add(this.rbInActive);
+            this.pnlStatus.Location = new System.Drawing.Point(114, 50);
+            this.pnlStatus.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pnlStatus.Name = "pnlStatus";
+            this.pnlStatus.Size = new System.Drawing.Size(209, 28);
+            this.pnlStatus.TabIndex = 10;
+            // 
+            // rbActive
+            // 
+            this.rbActive.AutoSize = true;
+            this.rbActive.Checked = true;
+            this.rbActive.Font = new System.Drawing.Font("Oswald Regular", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbActive.Location = new System.Drawing.Point(34, 1);
+            this.rbActive.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rbActive.Name = "rbActive";
+            this.rbActive.Size = new System.Drawing.Size(60, 24);
+            this.rbActive.TabIndex = 3;
+            this.rbActive.TabStop = true;
+            this.rbActive.Text = "Active";
+            this.rbActive.UseVisualStyleBackColor = true;
+            // 
+            // rbInActive
+            // 
+            this.rbInActive.AutoSize = true;
+            this.rbInActive.Font = new System.Drawing.Font("Oswald Regular", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbInActive.Location = new System.Drawing.Point(102, 1);
+            this.rbInActive.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.rbInActive.Name = "rbInActive";
+            this.rbInActive.Size = new System.Drawing.Size(70, 24);
+            this.rbInActive.TabIndex = 4;
+            this.rbInActive.Text = "Inactive";
+            this.rbInActive.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.Enabled = false;
+            this.textBox1.Font = new System.Drawing.Font("Oswald Regular", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(6, 22);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(107, 28);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = "Customer Type";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Oswald Regular", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Image = global::ROMS.Properties.Resources.close;
+            this.btnClose.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClose.Location = new System.Drawing.Point(249, 88);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(74, 34);
+            this.btnClose.TabIndex = 13;
+            this.btnClose.Text = "Close";
+            this.btnClose.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // grbForm
+            // 
+            this.grbForm.Controls.Add(this.textBox1);
+            this.grbForm.Controls.Add(this.btnClose);
+            this.grbForm.Controls.Add(this.txtCount);
+            this.grbForm.Controls.Add(this.btnSave);
+            this.grbForm.Controls.Add(this.txtStatus);
+            this.grbForm.Controls.Add(this.pnlStatus);
+            this.grbForm.Location = new System.Drawing.Point(12, 13);
+            this.grbForm.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.grbForm.Name = "grbForm";
+            this.grbForm.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.grbForm.Size = new System.Drawing.Size(337, 140);
+            this.grbForm.TabIndex = 15;
+            this.grbForm.TabStop = false;
+            // 
+            // CP_CustomerType
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(363, 166);
+            this.Controls.Add(this.grbForm);
+            this.Font = new System.Drawing.Font("Oswald Regular", 11.25F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "CP_CustomerType";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Customer Type";
+            this.Load += new System.EventHandler(this.CancelReason_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.CP_Brand_KeyDown);
+            this.Leave += new System.EventHandler(this.CP_Brand_Leave);
+            ((System.ComponentModel.ISupportInitialize)(this.errBrand)).EndInit();
+            this.pnlStatus.ResumeLayout(false);
+            this.pnlStatus.PerformLayout();
+            this.grbForm.ResumeLayout(false);
+            this.grbForm.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ErrorProvider errBrand;
+        public System.Windows.Forms.TextBox txtCount;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtStatus;
+        private System.Windows.Forms.Panel pnlStatus;
+        private System.Windows.Forms.RadioButton rbActive;
+        private System.Windows.Forms.RadioButton rbInActive;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.GroupBox grbForm;
+    }
+}
