aecee45c8cf4657ee3368ab0d67e184765993c5d
Sathis-PremKumar <sathish@shivasoftwares.com>
2025-12-24 11:06:35 +0530

    Product Info Load

--- ROMS/CP_Product_Info.cs
+++ ROMS/CP_Product_Info.cs
@@ -37,10 +37,7 @@ namespace ROMS
         {
             try
             {
-                if (e.KeyCode == Keys.F5)
-                {
 
-                }
             }
             catch (Exception ex)
             {
@@ -53,7 +50,53 @@ namespace ROMS
         {
             try
             {
-
+                lblPICode.Text = "";
+                lblPREName.Text = "";
+                lblPRTName.Text = "";
+                lblProductCategory.Text = "";
+                lblProductType.Text = "";
+                lblUnit.Text = "";
+                lblUPP.Text = "";
+                lblGroup.Text = "";
+                lblSubgroup.Text = "";
+                lblBrand.Text = "";
+                lblPurLocation.Text = "";
+                lblPurRack.Text = "";
+                lblSalesLocation.Text = "";
+                lblSalesRack.Text = "";
+                if (varProductId != 0)
+                {
+                    DataSet objDs = new DataSet();
+                    //**** To call the function from SP ***************
+                    SPDataService objdserv = new SPDataService();
+                    MR_Product objMR_Product = new MR_Product();
+                    objMR_Product.paraViewType = 89;
+                    objMR_Product.ParaProductCode = varProductId;
+                    SPDataService objspservice = new SPDataService();
+                    DataSet objDS;
+                    objDS = objdserv.udfnproductmasterlist(objMR_Product);
+                    objdserv.CloseConnection();
+                    if (objDS != null)
+                    {
+                        if (objDS.Tables[0].Rows.Count > 0)
+                        {
+                            lblPICode.Text = Convert.ToString(objDS.Tables[0].Rows[0]["PR_PICode"].ToString());
+                            lblPREName.Text = Convert.ToString(objDS.Tables[0].Rows[0]["PR_EName"].ToString());
+                            lblPRTName.Text = Convert.ToString(objDS.Tables[0].Rows[0]["PR_TName"].ToString());
+                            lblProductCategory.Text = Convert.ToString(objDS.Tables[0].Rows[0]["Category"].ToString());
+                            lblProductType.Text = Convert.ToString(objDS.Tables[0].Rows[0]["Product Type"].ToString());
+                            lblUnit.Text = Convert.ToString(objDS.Tables[0].Rows[0]["Unit"].ToString());
+                            lblUPP.Text = Convert.ToString(objDS.Tables[0].Rows[0]["UPP"].ToString());
+                            lblGroup.Text = Convert.ToString(objDS.Tables[0].Rows[0]["Group"].ToString());
+                            lblSubgroup.Text = Convert.ToString(objDS.Tables[0].Rows[0]["Subgroup"].ToString());
+                            lblBrand.Text = Convert.ToString(objDS.Tables[0].Rows[0]["Brand"].ToString());
+                            lblPurLocation.Text = Convert.ToString(objDS.Tables[0].Rows[0]["Pur_Location"].ToString());
+                            lblPurRack.Text = Convert.ToString(objDS.Tables[0].Rows[0]["Pur_Rack"].ToString());
+                            lblSalesLocation.Text = Convert.ToString(objDS.Tables[0].Rows[0]["Sales_Location"].ToString());
+                            lblSalesRack.Text = Convert.ToString(objDS.Tables[0].Rows[0]["Sales_Rack"].ToString());
+                        }
+                    }
+                }
             }
             catch (Exception ex)
             {
