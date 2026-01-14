f5477799c490c12af56e71b880cff7e7eb0a68d3
Sathis-PremKumar <sathish@shivasoftwares.com>
2026-01-14 10:11:39 +0530

    Product Approval Add Is a Sales Product CheckBox

--- ROMS/CP_ProductApproval.designer.cs
+++ ROMS/CP_ProductApproval.designer.cs
@@ -220,6 +220,7 @@
             this.grdSubgroup = new System.Windows.Forms.DataGridView();
             this.btnSubgroup = new System.Windows.Forms.Button();
             this.grpGoodsOutward = new System.Windows.Forms.GroupBox();
+            this.chkSalesProduct = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.epProductApproval)).BeginInit();
             this.tsStockTransferList.SuspendLayout();
             this.pnlGoodsOutward.SuspendLayout();
@@ -444,6 +445,7 @@
             // 
             // grplocation
             // 
+            this.grplocation.Controls.Add(this.chkSalesProduct);
             this.grplocation.Controls.Add(this.txtSalesPICode);
             this.grplocation.Controls.Add(this.textBox8);
             this.grplocation.Controls.Add(this.DGV_FilterSalesLocation);
@@ -961,7 +963,7 @@
             this.txtpicode.Location = new System.Drawing.Point(135, 26);
             this.txtpicode.MaxLength = 20;
             this.txtpicode.Name = "txtpicode";
-            this.txtpicode.Size = new System.Drawing.Size(427, 27);
+            this.txtpicode.Size = new System.Drawing.Size(315, 27);
             this.txtpicode.TabIndex = 0;
             this.txtpicode.Enter += new System.EventHandler(this.Txtpicode_Enter);
             this.txtpicode.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Txtpicode_KeyDown);
@@ -2253,6 +2255,20 @@
             this.grpGoodsOutward.TabIndex = 958819;
             this.grpGoodsOutward.TabStop = false;
             // 
+            // chkSalesProduct
+            // 
+            this.chkSalesProduct.AutoSize = true;
+            this.chkSalesProduct.Location = new System.Drawing.Point(456, 27);
+            this.chkSalesProduct.Name = "chkSalesProduct";
+            this.chkSalesProduct.Size = new System.Drawing.Size(106, 24);
+            this.chkSalesProduct.TabIndex = 1;
+            this.chkSalesProduct.Text = "Is a Sales Item";
+            this.chkSalesProduct.UseVisualStyleBackColor = true;
+            this.chkSalesProduct.CheckedChanged += new System.EventHandler(this.chkSalesProduct_CheckedChanged);
+            this.chkSalesProduct.Enter += new System.EventHandler(this.chkSalesProduct_Enter);
+            this.chkSalesProduct.KeyDown += new System.Windows.Forms.KeyEventHandler(this.chkSalesProduct_KeyDown);
+            this.chkSalesProduct.Leave += new System.EventHandler(this.chkSalesProduct_Leave);
+            // 
             // CP_ProductApproval
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 19F);
@@ -2484,5 +2500,6 @@
         private System.Windows.Forms.Button btnImgClose;
         private System.Windows.Forms.TextBox textBox8;
         private System.Windows.Forms.TextBox txtSalesPICode;
+        private System.Windows.Forms.CheckBox chkSalesProduct;
     }
 }
