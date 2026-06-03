42d1ea01b9a489f40b5580c7e3571918537af3b2
Sathis-PremKumar <sathish@shivasoftwares.com>
2026-06-03 18:28:19 +0530

    Report Changes

--- ROMS/Reports/RPT_Stock_TakingSheet.cs
+++ ROMS/Reports/RPT_Stock_TakingSheet.cs
@@ -287,6 +287,14 @@ namespace ROMS.Reports {
                 return this.DataDefinition.ParameterFields[20];
             }
         }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.Shared.IParameterField Parameter_paraReportFormat {
+            get {
+                return this.DataDefinition.ParameterFields[21];
+            }
+        }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
