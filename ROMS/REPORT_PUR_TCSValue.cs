1e41864835f439b804f6b1ad969eded4920a532f
Sathis-PremKumar <sathish@shivasoftwares.com>
2026-07-16 19:02:11 +0530

    Changes

--- ROMS/REPORT_PUR_TCSValue.cs
+++ ROMS/REPORT_PUR_TCSValue.cs
@@ -208,7 +208,7 @@ namespace ROMS
                 int varPrint = 0;
                 DataSet objDs = new DataSet();
                 SPDataService objdserv = new SPDataService();
-                objDs = objdserv.udfnPurHsnReport(varViewType, Convert.ToInt32(cmbSupplierType.SelectedValue), "", 0, dpFromDate.Text, dpToDate.Text, 0, 0, 0, 0, 0, 0, varSupplierId, varScheduleId, Convert.ToInt32(cmbInvType.SelectedValue), 0, 0, 0, 0, "",varMonthIds, 0, 0, 0, 0, 0, 0, "");
+                objDs = objdserv.udfnPurHsnReport("TRNG_Purchase_Reports_23_28", varViewType, Convert.ToInt32(cmbSupplierType.SelectedValue), "", 0, dpFromDate.Text, dpToDate.Text, 0, 0, 0, 0, 0, 0, varSupplierId, varScheduleId, Convert.ToInt32(cmbInvType.SelectedValue), 0, 0, 0, 0, "",varMonthIds, 0, 0, 0, 0, 0, 0, "");
                 objdserv.CloseConnection();
                 if (objDs != null) { if (objDs.Tables.Count > 0) { if (objDs.Tables[0].Rows.Count > 0) { varPrint = 1; } } }
                 string varReportName = "";
