1d2b94ed297a3b13a58ce79a5ac5699cd631d85c
Sathis-PremKumar <sathish@shivasoftwares.com>
2026-09-09 16:36:28 +0530

    Details Report Changes

--- ROMS/REPORT_SALES_SummaryDetail.cs
+++ ROMS/REPORT_SALES_SummaryDetail.cs
@@ -95,7 +95,7 @@ namespace ROMS
                     if (Convert.ToInt32(cmbSalesType.SelectedValue) == 585)
                     {
                         RPTViewer.ReportSource = null;
-                        RPTViewer.Visible = true;
+                        RPTViewer.Visible = false;
                         RPTViewer.BringToFront();
                         lblNoRecordsFound.Visible = true;
                         lblNoRecordsFound.BringToFront();
